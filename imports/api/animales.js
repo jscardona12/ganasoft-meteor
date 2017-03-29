791572fc3dc81ac6c45c6b75295e5b9958614ade
Juan Sebastian Cardona <js.cardona12@uniandes.edu.co>
2017-03-29 15:21:42 -0500

    animal server fix

--- imports/api/animales.js
+++ imports/api/animales.js
@@ -1,60 +1,70 @@
-
 /**s
  * Created by Juan on 16/03/2017.
  */
 
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
+import {Meteor} from 'meteor/meteor';
+import {Mongo} from 'meteor/mongo';
+import {check} from 'meteor/check';
 
 export const Animales = new Mongo.Collection('animales');
 
 if (Meteor.isServer) {
-  Meteor.publish('animales', function animalsPublication(finca) {
-    return Animales.find({
-      $or: [
-        { farm: finca },
-      ],
+    Meteor.publish('animales', function animalsPublication() {
+        return Animales.find({
+            $or: [
+                {owner: this.userId},
+            ],
+        });
     });
-  });
 }
 
 Meteor.methods({
-  'animales.insert'(farm, number, especie, raza, sexo, descripcion) {
+    'animales.insert'(farm, number, especie, raza, sexo, descripcion) {
 
-    check(number, Number);
-    check(especie, String);
-    check(raza, String);
-    check(sexo, String);
-    check(descripcion, String);
+        console.log("ATRIBUTOS");
+        console.log(farm);
+        console.log(number);
+        console.log(especie);
+        console.log(raza);
+        console.log(sexo);
+        console.log(descripcion);
 
-    // Make sure the user is logged in before inserting a task
-    if (! this.userId) {
-      throw new Meteor.Error('not-authorized');
-    }
-
-    Animales.insert({
-      farm,
-      number,
-      especie,
-      raza,
-      sexo,
-      descripcion,
-      fechaNacimiento: new Date(),
-      owner: this.userId
-    });
-  },
-  'animales.remove'(animalId) {
-    console.log(animalId);
-    //check(taskId, String);
+        check(number, Number);
+        check(especie, String);
+        check(raza, String);
+        check(sexo, String);
+        check(descripcion, String);
 
 
-    const task = Animales.findOne(animalId);
-    if (task.owner !== this.userId) {
-      // If the task is private, make sure only the owner can delete it
-      throw new Meteor.Error('not-authorized');
-    }
 
-    Animales.remove(animalId);
-  },
+        // Make sure the user is logged in before inserting a task
+        if (!this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Animales.insert({
+            farm,
+            number,
+            especie,
+            raza,
+            sexo,
+            descripcion,
+            fechaNacimiento: new Date(),
+            owner: this.userId
+        });
+    },
+
+    'animales.remove'(animalId) {
+        console.log(animalId);
+        //check(taskId, String);
+
+
+        const task = Animales.findOne(animalId);
+        if (task.owner !== this.userId) {
+            // If the task is private, make sure only the owner can delete it
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Animales.remove(animalId);
+    },
 });
--- imports/ui/Animal.jsx
+++ imports/ui/Animal.jsx
@@ -12,6 +12,11 @@ class Animal extends Component {
         super(props);
 
         this.state = {
+            number:1,
+            especie: '',
+            raza:'',
+            sexo:'',
+            descripcion:''
         };
     }
 
@@ -22,12 +27,43 @@ class Animal extends Component {
     getPath() {
         return "/" + this.props.params.idfinca + "/animales"
     }
+    insertAnimal()
+    {
+        console.log(FlowRouter.getParam('fincaId'))
+        Meteor.call('animales.insert',FlowRouter.getParam('fincaId'),this.state.number,this.state.especie,
+            this.state.raza,this.state.sexo,this.state.descripcion);
+    }
+
 
 
     render() {
         return (
             <div>
                 {console.log(this.props.animales)}
+
+                Number:
+                <input type="text" value={this.state.number} onChange={(event) => {
+                    this.setState({number: event.target.values})
+                }} />
+
+                Especie:
+                <input type="text" value={this.state.especie} onChange={(event) => {
+                    this.setState({especie: event.target.value})
+                }} />
+                Raza:
+                <input type="text" value={this.state.raza} onChange={(event) => {
+                    this.setState({raza: event.target.value})
+                }} />
+                Genero:
+                <input type="text" value={this.state.sexo} onChange={(event) => {
+                    this.setState({sexo: event.target.value})
+                }} />
+
+                Descripcion:
+                <input type="text" value={this.state.descripcion} onChange={(event) => {
+                    this.setState({descripcion: event.target.value})
+                }} />
+                <button onClick={this.insertAnimal.bind(this)}> insertar animal </button>
                 {this.props.animales.map((animal, index)=>
                     <div key ={index} className="panel panel-info col-md-3">
                         <div className="panel-heading">
@@ -79,7 +115,7 @@ export default createContainer(() => {
     var idFinca = FlowRouter.getParam("fincaId");
     console.log(idFinca);
     return {
-        animales: Animales.find({farm:idFinca,owner:Meteor.userId()}).fetch(),
+        animales: Animales.find({farm:idFinca}).fetch(),
         currentUser: Meteor.user()
     };
 
