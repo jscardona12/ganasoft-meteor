69c9328bd95b01b21f27cbd231f57372003ec7d9
nicbuitr <n.buitrago29@uniandes.edu.co>
2017-03-28 21:59:46 -0500

    Term update
    Updated "Administrate" to "Manage".

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -9,7 +9,7 @@ class Home extends Component {
                 <div className="col-md-2"></div>
                 <div className="col-md-10 text-vertical-center">
                     <h1 >Welcome to GanaSoft</h1>
-                    <h2> GanaSoft is a Web Page where you can administrate your farms </h2>
+                    <h2> GanaSoft is a Web Page where you can manage your farms </h2>
                 </div>
             </div>
         );
